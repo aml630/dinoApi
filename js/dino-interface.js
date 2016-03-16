1cf38d0243f399cfb58f516449f78657dc0a02f6
Alex Larson and Michael Smith <michaelanthonysmith00@gmail.com>
2016-03-16 09:13:29 -0700

    add basic dino functionality

--- js/dino-interface.js
+++ js/dino-interface.js
@@ -1,1 +1,29 @@
 console.log("hey there");
+var getDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=3'),
+  fillContainer = function(html) {
+    $('#dino').html(html);
+  },
+  error = function() {
+    console.log("no dinos for you");
+  };
+
+  getDinos.then(fillContainer, error);
+
+
+
+  $(document).ready(function(){
+    $('#dinoForm').submit(function(event){
+      event.preventDefault();
+      var numOfParagraphs = $('#numOfParagraphs').val();
+      var wordsPerParagraph = $('#wordsPerParagraph').val();
+      var customDinos = $.get('http://dinoipsum.herokuapp.com/api/?format=html&paragraphs=' + numOfParagraphs + '&words=' + wordsPerParagraph);
+      fillContainer = function(html) {
+        $('#userDino').html(html);
+      },
+      error = function() {
+        console.log("no dinos for you");
+      };
+      customDinos.then(fillContainer, error);
+
+    });
+  });
